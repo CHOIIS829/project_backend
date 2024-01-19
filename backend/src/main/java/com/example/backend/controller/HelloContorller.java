7a09c0b3db46762800e286952021457203bd5300
tromxx <93820349+tromxx@users.noreply.github.com>
2024-01-19 15:44:11 +0900

    commit

--- backend/src/main/java/com/example/backend/controller/HelloContorller.java
+++ backend/src/main/java/com/example/backend/controller/HelloContorller.java
@@ -1,13 +1,22 @@
 package com.example.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
+@Slf4j
 public class HelloContorller {
 
-    @GetMapping("/hello")
-    public String hello(){
+    @GetMapping("/test")
+    public String getData() {
         return "hello world!!!";
     }
+
+    @PostMapping("/test")
+    public Boolean postData(@RequestBody String val) {
+        log.info(val);
+        return val != null;
+    }
+
 }
