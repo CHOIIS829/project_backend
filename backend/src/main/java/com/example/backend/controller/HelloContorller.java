f45bcce6ddb400b2bf124cd385a80491909e2065
jjaggu829 <cth7097@gmail.com>
2024-01-19 15:09:02 +0900

    TEST Controller

--- backend/src/main/java/com/example/backend/controller/HelloContorller.java
+++ backend/src/main/java/com/example/backend/controller/HelloContorller.java
@@ -1,13 +1,34 @@
 package com.example.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class HelloContorller {
 
     @GetMapping("/hello")
     public String hello(){
         return "hello world!!!";
+    }
+
+
+    @GetMapping("/test")
+    public String get(){
+        return "response Get!";
+    }
+
+    @PostMapping("/test")
+    public String post(){
+        return "response Post!!";
+    }
+
+    @PutMapping("/test")
+    public String put(){
+        return "response Put!!";
+    }
+
+    @DeleteMapping("/test")
+    public String delete(){
+        return "response Delete!!";
     }
 }
