f78289af1e8ce97ce5155eae13af323f990786d1
jjaggu829 <cth7097@gmail.com>
2024-02-05 17:15:12 +0900

    Update MemberController.java

--- backend/src/main/java/com/example/backend/controller/MemberController.java
+++ backend/src/main/java/com/example/backend/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
-public class MemberContorller {
+public class MemberController {
 
     private final MemberService memberService;
 
