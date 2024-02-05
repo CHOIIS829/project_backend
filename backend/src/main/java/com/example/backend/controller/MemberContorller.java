35eb6fec7800a3cd46bff5747f785b1258d3c072
jjaggu829 <cth7097@gmail.com>
2024-02-05 15:54:56 +0900

    테스트 회원가입 api 작성

---
+++ backend/src/main/java/com/example/backend/controller/MemberContorller.java
@@ -0,0 +1,29 @@
+package com.example.backend.controller;
+
+import com.example.backend.domain.entity.Member;
+import com.example.backend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberContorller {
+
+    private final MemberService memberService;
+
+    @PostMapping("/singup")
+    public ResponseEntity<String> singUp(@RequestBody Member m){
+        try {
+            Member saveMember = memberService.signUp(m);
+            return new ResponseEntity<>("회원가입 성공", HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>("회원가입 실패", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
