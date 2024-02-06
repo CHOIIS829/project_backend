fe53cafec2bdc2a5f3488d808d69139cfc3a7d36
jjaggu829 <cth7097@gmail.com>
2024-02-06 15:17:58 +0900

    tt

--- backend/src/main/java/com/example/backend/controller/HelloContorller.java
+++ backend/src/main/java/com/example/backend/controller/HelloContorller.java
@@ -4,12 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+//@CrossOrigin("http://localhost:3000")
 @Slf4j
 public class HelloContorller {
 
     @GetMapping("/test")
-<<<<<<< HEAD
     public String getData() {
         return "hello world!!!";
     }
@@ -19,25 +18,4 @@ public class HelloContorller {
         return val != null;
     }
 
-    @GetMapping("/test")
-=======
->>>>>>> f78289af1e8ce97ce5155eae13af323f990786d1
-    public String get(){
-        return "response Get!";
-    }
-
-    @PostMapping("/test")
-    public String post(){
-        return "response Post!!";
-    }
-
-    @PutMapping("/test")
-    public String put(){
-        return "response Put!!";
-    }
-
-    @DeleteMapping("/test")
-    public String delete(){
-        return "response Delete!!";
-    }
 }
